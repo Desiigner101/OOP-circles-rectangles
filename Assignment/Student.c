cfb6fab84c949a5b97b54a2d4678f9f177088178
Gino Sarsonas <desiigner4074@gmail.com>
2024-09-25 04:54:40 -0700

    Product.c only

--- Assignment/Student.c
+++ Assignment/Student.c
@@ -1,85 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-typedef struct student{
-	char name[50];
-	int idno;
-	int age;
-}stude;
+typedef struct student {
+    char name[50];
+    int idno;
+    int age;
+} stude;
 
 void display(stude s[], int size);
-void inputStudent(stude s[], int *size) ;
+void inputStudent(stude s[], int *size);
 
 int main(int argc, char *argv[]) {
-	stude s[3];
-	int choice;
-	int ctr=0;
-	//printf("%s",s[1].name);
-	do{
-		printf("1 - Add Student\n");
-		printf("2 - Display\n");
-		printf("3 - Exit\n");
-		printf("Enter choice: ");
-		scanf("%d",&choice);
-		
-		switch(choice){
-			case 1:
-					inputStudent(s,&ctr);
-					break;
-			case 2:
-					display(s,ctr);
-					break;
-		}
-	}while(choice !=3);
-	
-	return 0;
+    stude s[3];
+    int choice;
+    int ctr = 0;
+
+    do {
+        printf("1 - Add Student\n");
+        printf("2 - Display\n");
+        printf("3 - Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                inputStudent(s, &ctr);
+                break;
+            case 2:
+                display(s, ctr);
+                break;
+        }
+    } while (choice != 3);
+
+    return 0;
 }
 
 void display(stude s[], int size) {
-    int i, j;
-    int alreadyDisplayed[size]; 
-    int count = 0; 
+    int x;
+    for (x = 0; x < size; x++) {
+        printf("Name: %s\t", s[x].name);
+        printf("Id Number: %d\t", s[x].idno);
+        printf("Age: %d\t", s[x].age);
+        printf("\n");
+    }
+}
 
-    for (i = 0; i < size; i++) {
+void inputStudent(stude s[], int *size) {
+    if (*size == 3) {
+        printf("Array is full.\n");
+    } else {
+        int id, i;
+        char name[50];
         int duplicate = 0;
-        for (j = 0; j < count; j++) {
-            if (s[i].idno == alreadyDisplayed[j]) {
+
+        printf("Enter ID #: ");
+        scanf("%d", &id);
+
+        for (i = 0; i < *size; i++) {
+            if (s[i].idno == id) {
                 duplicate = 1;
                 break;
             }
         }
 
-       
-        if (!duplicate) {
-            printf("Name: %s\t", s[i].name);
-            printf("Id Number: %d\t", s[i].idno);
-            printf("Age: %d\t", s[i].age);
-            printf("\n");
-            alreadyDisplayed[count] = s[i].idno;
-            count++;
+        if (duplicate) {
+            printf("Error: A student with ID number %d already exists.\n", id);
+        } else {
+            s[*size].idno = id; 
+            printf("Enter name: ");
+            scanf("%s", name);
+            strcpy(s[*size].name, name);
+
+            printf("Enter age: ");
+            scanf("%d", &s[*size].age);
+
+            (*size)++;
         }
     }
-}
-
-
-
-void inputStudent(stude s[], int *size){
-	char name[50];
-	if(*size == 3)
-		printf("Array is full.\n");
-	else{
-		printf("Enter name: ");
-		scanf("%s",name);
-		strcpy(s[*size].name,name);
-
-		printf("Enter ID #: ");
-		scanf("%d",&s[*size].idno);
-
-		printf("Enter age: ");
-		scanf("%d",&s[*size].age);
-		(*size)++;
-	}
-	
 }
